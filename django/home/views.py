6ca57bd44f30eb6fb27f41d157e33bc8088bb5ed
Roniel <gustavoroniel@gmail.com>
2024-05-17 20:23:01 -0300

    update learning

--- django/home/views.py
+++ django/home/views.py
@@ -5,5 +5,5 @@ from django.template import loader
 
 
 def home(request):
-    template = loader.get_template('home')
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
